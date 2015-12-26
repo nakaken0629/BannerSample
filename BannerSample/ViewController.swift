f9275f0303ee7c042f47b98fb3f473deacce5ae9
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-12-26 21:27:04 +0900

    縦画面のバナーを追加

--- BannerSample/ViewController.swift
+++ BannerSample/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var items: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for(var i = 0; i < 50; i++) {
+            items.append("アイテム \(i)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.items[indexPath.item]
+        return cell
+    }
 }
 
